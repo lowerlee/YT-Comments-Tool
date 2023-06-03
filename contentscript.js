3cf32b92b328b0762d1f35f6786bbc5d5d49929e
lowerlee <87386953+lowerlee@users.noreply.github.com>
2023-06-03 12:10:28 -0700

    Update contentscript.js

--- contentscript.js
+++ contentscript.js
@@ -48,117 +48,90 @@ function getYouTubeVideoId() {
   // Add event listener to the search button
   searchButton.addEventListener('click', searchComments);
  });
- 
- // Function to search comments
- function searchComments() {
-  const searchTerm = searchInput.value.trim().toLowerCase();
 
-  // Clear previous search results
-  commentsContainer.innerHTML = '';
+// Variable to store the comments
+let comments = [];
 
-  function timeAgo(date) {
-    var seconds = Math.floor((new Date() - date) / 1000);
+// Fetch comments when the page loads
+fetchComments();
 
-    var interval = seconds / 31536000;
+// Function to search comments
+function searchComments() {
+    const searchTerm = searchInput.value.trim().toLowerCase();
 
-    if (interval > 1) {
-        return Math.floor(interval) + " years ago";
-    }
-    interval = seconds / 2592000;
-    if (interval > 1) {
-        return Math.floor(interval) + " months ago";
-    }
-    interval = seconds / 604800;
-    if (interval > 1) {
-        return Math.floor(interval) + " weeks ago";
-    }
-    interval = seconds / 86400;
-    if (interval > 1) {
-        return Math.floor(interval) + " days ago";
-    }
-    interval = seconds / 3600;
-    if (interval > 1) {
-        return Math.floor(interval) + " hours ago";
-    }
-    interval = seconds / 60;
-    if (interval > 1) {
-        return Math.floor(interval) + " minutes ago";
-    }
-    return Math.floor(seconds) + " seconds ago";
-  }
+    // Clear previous search results
+    commentsContainer.innerHTML = '';
 
-  // Fetch comments using the YouTube API script
-  var videoId = getYouTubeVideoId();
-  var apiKey = 'AIzaSyBXXFXlhx29wNP2egXR4IvKmSTH5h9nyZM'; // replace with your API key
-
-  function fetchComments(pageToken, currentCount = 0) {
-    var url = 'https://www.googleapis.com/youtube/v3/commentThreads?part=snippet,replies&videoId=' + videoId + '&key=' + apiKey;
-
-    if (pageToken) {
-      url += '&pageToken=' + pageToken;
-    }
-
-    // Fetch the comments using the YouTube API
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      // data.items contains the comment threads
-      for (var i = 0; i < data.items.length; i++) {
-        var comment = data.items[i].snippet.topLevelComment.snippet.textOriginal.toLowerCase();
+    // Filter the stored comments based on the search term
+    for (let i = 0; i < comments.length; i++) {
+        let comment = comments[i].snippet.topLevelComment.snippet.textOriginal.toLowerCase();
         if (comment.includes(searchTerm)) {
-          var commentElement = document.createElement('p');
-          commentElement.innerText = comment;
-
-          // Add author's profile image, profile name, and update date
-          var authorImage = document.createElement('img');
-          authorImage.src = data.items[i].snippet.topLevelComment.snippet.authorProfileImageUrl;
-          authorImage.className = 'author-thumbnail';
-
-          var authorName = document.createElement('p');
-          authorName.innerText = data.items[i].snippet.topLevelComment.snippet.authorDisplayName;
-          authorName.className = 'author-text';
-          
-          var updateDate = document.createElement('p');
-          updateDate.innerText = timeAgo(new Date(data.items[i].snippet.topLevelComment.snippet.updatedAt));
-          updateDate.className = 'published-time-text';
-          
-          var authorTextContainer = document.createElement('div');
-          authorTextContainer.className = 'author-text-container';
-          authorTextContainer.appendChild(authorName);
-          authorTextContainer.appendChild(updateDate);
-
-          var textContainer = document.createElement('div');
-          textContainer.appendChild(authorTextContainer);
-          textContainer.appendChild(commentElement);
-
-          var commentContainer = document.createElement('div');
-          commentContainer.className = 'comment-filter-comment';
-          commentContainer.appendChild(authorImage);
-          commentContainer.appendChild(textContainer);
-          commentsContainer.appendChild(commentContainer);
+            // Create and append the comment element as before...
         }
-      }
+    }
+}
 
-      currentCount += data.items.length;
-      commentCountElement.textContent = 'Comments loaded: ' + currentCount;
+// Function to calculate time ago
+function timeAgo(date) {
+  var seconds = Math.floor((new Date() - date) / 1000);
+  var interval = seconds / 31536000;
 
-      // If there's a next page, fetch it
-      if (data.nextPageToken) {
-        fetchComments(data.nextPageToken, currentCount);
-      }
-      
-      else {
-        commentCountElement.textContent = 'Total comments: ' + currentCount;
-      }
-    })
-    .catch(error => console.error('Error:', error));
+  if (interval > 1) {
+      return Math.floor(interval) + " years ago";
   }
-  // Start fetching comments
-  fetchComments();
- }
+  interval = seconds / 2592000;
+  if (interval > 1) {
+      return Math.floor(interval) + " months ago";
+  }
+  interval = seconds / 604800;
+  if (interval > 1) {
+      return Math.floor(interval) + " weeks ago";
+  }
+  interval = seconds / 86400;
+  if (interval > 1) {
+      return Math.floor(interval) + " days ago";
+  }
+  interval = seconds / 3600;
+  if (interval > 1) {
+      return Math.floor(interval) + " hours ago";
+  }
+  interval = seconds / 60;
+  if (interval > 1) {
+      return Math.floor(interval) + " minutes ago";
+  }
+  return Math.floor(seconds) + " seconds ago";
+}
+
+// Fetch comments using the YouTube API script
+function fetchComments(pageToken, currentCount = 0) {
+  var url = 'https://www.googleapis.com/youtube/v3/commentThreads?part=snippet,replies&videoId=' + videoId + '&key=' + apiKey;
+
+  if (pageToken) {
+      url += '&pageToken=' + pageToken;
+  }
+
+  // Fetch the comments using the YouTube API
+  fetch(url)
+      .then(response => response.json())
+      .then(data => {
+          // data.items contains the comment threads
+          for (var i = 0; i < data.items.length; i++) {
+              // Push the comment to the comments array
+              comments.push(data.items[i]);
+          }
+
+          currentCount += data.items.length;
+
+          // If there's a next page, fetch it
+          if (data.nextPageToken) {
+              fetchComments(data.nextPageToken, currentCount);
+          }
+      })
+      .catch(error => console.error('Error:', error));
+}
  
- // Function to wait for an element to be available in the DOM
- function waitForElement(selector, callback) {
+// Function to wait for an element to be available in the DOM
+function waitForElement(selector, callback) {
   const element = document.querySelector(selector);
   if (element) {
     callback(element);
@@ -167,4 +140,4 @@ function getYouTubeVideoId() {
   else {
     setTimeout(() => waitForElement(selector, callback), 100);
   }
- }
+}
